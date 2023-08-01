c30a7fbd98f11862b964c0ca54bfdde5bea1ae15
Ronisha Shrestha <ronisha.shrestha03gmail.com>
2023-08-01 13:39:54 +0545

    shopping list update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import { ShoppingListItem } from "./components/ShoppingListItem";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const handleChange = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (newItem !== "" && !items.includes(newItem)) {
+      setItems((oldItems) => [newItem, ...oldItems]);
+      setNewItem("");
+    } else {
+      window.alert("Cannot add the given element.");
+    }
+  };
+
+  const deleteItem = (index) => {
+    setItems((oldItems) => oldItems.filter((item, i) => i !== index));
+  };
+
   return (
     <div className="container">
       <h1 className="mb-4">My Shopping List</h1>
@@ -11,13 +32,21 @@ function App() {
           type="text"
           placeholder="E.g. Carrots"
           className="v__input flex-1"
+          value={newItem}
+          onChange={handleChange}
         />
-        <button className="v__button">Add</button>
+        <button className="v__button" onClick={handleAddItem}>
+          Add
+        </button>
       </div>
       <div className="v__list-container overflow-y-scroll">
-        {/* Map your data here: */}
-        <ShoppingListItem />
-        <ShoppingListItem />
+        {items.map((item, index) => (
+          <ShoppingListItem
+            key={index}
+            item={item}
+            deleteItem={() => deleteItem(index)}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/ShoppingListItem.jsx
+++ src/components/ShoppingListItem.jsx
@@ -1,11 +1,30 @@
+import React, { useState } from "react";
 import classes from "./ShoppingListItem.module.css";
 
-export const ShoppingListItem = () => {
+export const ShoppingListItem = (props) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleToggleChecked = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div className="flex items-center p-2">
-      <input type="checkbox" className="mr-2" />
-      <h3 className="flex-1">Carrots</h3>
-      <button className={classes.removeButton}>x</button>
+    <div
+      className={`flex items-center p-2 ${isChecked && classes.checkedItem}`}
+    >
+      <input
+        type="checkbox"
+        className="mr-2"
+        value={props.item}
+        checked={isChecked}
+        onChange={handleToggleChecked}
+      />
+      <h3 className={`flex-1 ${isChecked && classes.checkedText}`}>
+        {props.item}
+      </h3>
+      <button className={classes.removeButton} onClick={props.deleteItem}>
+        x
+      </button>
     </div>
   );
 };
